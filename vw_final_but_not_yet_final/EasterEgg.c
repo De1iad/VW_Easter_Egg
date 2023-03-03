b982fd8ce7b7beefbfa635789049107849b10014
obibby <obibby@student.42wolfsburg.de>
2023-03-03 09:30:29 +0000

    remove timer, add init

--- vw_final_but_not_yet_final/EasterEgg.c
+++ vw_final_but_not_yet_final/EasterEgg.c
@@ -12,14 +12,6 @@
 
 inputsEE EasterEggLightsEE;
 
-time_t	get_time_in_ms(void)
-{
-	struct timeval	tv;
-
-	gettimeofday(&tv, NULL);
-	return ((tv.tv_sec * 1000) + (tv.tv_usec * 0.001));
-}
-
 int	hex_to_dec(char *hex)
 {
 	int	dec;
@@ -43,50 +35,49 @@ int	hex_to_dec(char *hex)
 	return (dec);
 }
 
-void	set_lights(void)
+void	EasterEgg_Cyclic_10ms(void)
 {
-	FILE	*input = fopen("lights", "r");
-	time_t	previous = get_time_in_ms();
-	time_t	current = get_time_in_ms();
+	FILE	*input;
+	static	uint8_t	init;
 	char	buff[3];
 
-	current = get_time_in_ms();
-	if (current - previous >= 10)
+	if (!init)
 	{
-		EasterEggLightsEE.AmbientLights = fgetc(input) - '0';
-		fread(buff, sizeof(char), 3, input);
-		EasterEggLightsEE.AmbientLightsPWM = hex_to_dec(buff);
-		EasterEggLightsEE.BlinkLightLeft = fgetc(input) - '0';
-		fread(buff, sizeof(char), 3, input);
-		EasterEggLightsEE.BlinkLightLeftPWM = hex_to_dec(buff);
-		EasterEggLightsEE.BlinkLightRight = EasterEggLightsEE.BlinkLightLeft;
-		EasterEggLightsEE.BlinkLightRightPWM = EasterEggLightsEE.BlinkLightLeftPWM;
-		EasterEggLightsEE.BrakeLights = fgetc(input) - '0';
-		fread(buff, sizeof(char), 3, input);
-		EasterEggLightsEE.BrakeLightsPWM = hex_to_dec(buff);
-		EasterEggLightsEE.FogLights = fgetc(input) - '0';
-		fread(buff, sizeof(char), 3, input);
-		EasterEggLightsEE.FogLightsPWM = hex_to_dec(buff);
-		EasterEggLightsEE.FrontLights = fgetc(input) - '0';
-		fread(buff, sizeof(char), 3, input);
-		EasterEggLightsEE.LicensePlateLight1 = fgetc(input) - '0';
-		fread(buff, sizeof(char), 3, input);
-		EasterEggLightsEE.LicensePlateLight1PWM = hex_to_dec(buff);
-		EasterEggLightsEE.LicensePlateLight2 = EasterEggLightsEE.LicensePlateLight1;
-		EasterEggLightsEE.LicensePlateLight2PWM = EasterEggLightsEE.LicensePlateLight1PWM;
-		EasterEggLightsEE.LicensePlateLight3 = fgetc(input) - '0';
-		fread(buff, sizeof(char), 3, input);
-		EasterEggLightsEE.LicensePlateLight3PWM = hex_to_dec(buff);
-		EasterEggLightsEE.LicensePlateLight4 = EasterEggLightsEE.LicensePlateLight3;
-		EasterEggLightsEE.LicensePlateLight4PWM = EasterEggLightsEE.LicensePlateLight3PWM;
-		EasterEggLightsEE.ParkingLightLeft = fgetc(input) - '0';
-		fread(buff, sizeof(char), 3, input);
-		EasterEggLightsEE.ParkingLightLeftPWM = hex_to_dec(buff);
-		EasterEggLightsEE.ParkingLightRight = EasterEggLightsEE.ParkingLightLeft;
-		EasterEggLightsEE.ParkingLightRightPWM = EasterEggLightsEE.ParkingLightLeftPWM;
-		EasterEggLightsEE.ReverseLights = fgetc(input) - '0';
-		fread(buff, sizeof(char), 3, input);
-		EasterEggLightsEE.ReverseLightsPWM = hex_to_dec(buff);
-		previous = current;
+		input = fopen("lights", "r");
+		init = 1;
 	}
+	EasterEggLightsEE.AmbientLights = fgetc(input) - '0';
+	fread(buff, sizeof(char), 3, input);
+	EasterEggLightsEE.AmbientLightsPWM = hex_to_dec(buff);
+	EasterEggLightsEE.BlinkLightLeft = fgetc(input) - '0';
+	fread(buff, sizeof(char), 3, input);
+	EasterEggLightsEE.BlinkLightLeftPWM = hex_to_dec(buff);
+	EasterEggLightsEE.BlinkLightRight = EasterEggLightsEE.BlinkLightLeft;
+	EasterEggLightsEE.BlinkLightRightPWM = EasterEggLightsEE.BlinkLightLeftPWM;
+	EasterEggLightsEE.BrakeLights = fgetc(input) - '0';
+	fread(buff, sizeof(char), 3, input);
+	EasterEggLightsEE.BrakeLightsPWM = hex_to_dec(buff);
+	EasterEggLightsEE.FogLights = fgetc(input) - '0';
+	fread(buff, sizeof(char), 3, input);
+	EasterEggLightsEE.FogLightsPWM = hex_to_dec(buff);
+	EasterEggLightsEE.FrontLights = fgetc(input) - '0';
+	fread(buff, sizeof(char), 3, input);
+	EasterEggLightsEE.LicensePlateLight1 = fgetc(input) - '0';
+	fread(buff, sizeof(char), 3, input);
+	EasterEggLightsEE.LicensePlateLight1PWM = hex_to_dec(buff);
+	EasterEggLightsEE.LicensePlateLight2 = EasterEggLightsEE.LicensePlateLight1;
+	EasterEggLightsEE.LicensePlateLight2PWM = EasterEggLightsEE.LicensePlateLight1PWM;
+	EasterEggLightsEE.LicensePlateLight3 = fgetc(input) - '0';
+	fread(buff, sizeof(char), 3, input);
+	EasterEggLightsEE.LicensePlateLight3PWM = hex_to_dec(buff);
+	EasterEggLightsEE.LicensePlateLight4 = EasterEggLightsEE.LicensePlateLight3;
+	EasterEggLightsEE.LicensePlateLight4PWM = EasterEggLightsEE.LicensePlateLight3PWM;
+	EasterEggLightsEE.ParkingLightLeft = fgetc(input) - '0';
+	fread(buff, sizeof(char), 3, input);
+	EasterEggLightsEE.ParkingLightLeftPWM = hex_to_dec(buff);
+	EasterEggLightsEE.ParkingLightRight = EasterEggLightsEE.ParkingLightLeft;
+	EasterEggLightsEE.ParkingLightRightPWM = EasterEggLightsEE.ParkingLightLeftPWM;
+	EasterEggLightsEE.ReverseLights = fgetc(input) - '0';
+	fread(buff, sizeof(char), 3, input);
+	EasterEggLightsEE.ReverseLightsPWM = hex_to_dec(buff);
 }
